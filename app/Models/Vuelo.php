3fbe55508d0c9311f9c1f3c46a3c2407802ccf45
Cristian Quintero Garcia <cristian181199@gmail.com>
2022-01-12 23:19:49 +0100

    Migraciones y Modelos de Vuelos, Reservas, Aeropuertos (revisar modelo de Vuelo, en concreto el metodo destino)

---
+++ app/Models/Vuelo.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vuelo extends Model
+{
+    use HasFactory;
+
+    public function reservas()
+    {
+        return $this->hasMany(Reserva::class);
+    }
+
+    public function origen()
+    {
+        return $this->belongsTo(Aeropuerto::class, 'id');
+    }
+
+    public function destino()
+    {
+        return $this->belongsTo(Aeropuerto::class, 'id');
+    }
+
+}
