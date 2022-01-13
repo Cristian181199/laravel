4474634570a84e8228f4d5abb67edd969c9c46b3
Cristian Quintero <cristian181199@gmail.com>
2022-01-13 19:30:25 +0100

    13 enero

---
+++ app/Models/Factura.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Factura extends Model
+{
+    use HasFactory;
+
+    public function cliente()
+    {
+        return $this->belongsTo(Cliente::class);
+
+    }
+
+    public function lineas()
+    {
+        return $this->hasMany(Linea::class);
+    }
+
+}
