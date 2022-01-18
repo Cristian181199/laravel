10aabeef401234d90b7266fcad4e71a7a2f123fa
Cristian Quintero Garcia <cristian181199@gmail.com>
2022-01-18 11:52:56 +0100

    Migraciones y Modelos de cliente, factura, etc

---
+++ app/Models/Cliente.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cliente extends Model
+{
+    use HasFactory;
+
+    public function facturas()
+    {
+        return $this->hasMany(Factura::class);
+    }
+
+    // Esta relacion no la acabo de entender del todo
+    public function lineas()
+    {
+        return $this->hasManyThrough(Linea::class, Factura::class);
+    }
+}
